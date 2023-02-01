c51e2ea85829b4ab59621d6a3c3797885a919e9e
mantiniDev <pmantinidev@gmail.com>
2023-02-01 11:54:53 -0300

    bootstrapp21

--- gestao_pacientes/settings.py
+++ gestao_pacientes/settings.py
@@ -129,6 +129,10 @@ LOGIN_REDIRECT_URL = 'person_list'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_DIRS = [
+    'statics',
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
